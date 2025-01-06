3220f935d8c46d999de671c48f8fb26d1375bb0f
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-01-06 20:15:42 +0530

    insertion sort

---
+++ insertion sort.java
@@ -0,0 +1,28 @@
+class insertion{
+    void insert(int arr[], int i)
+    {
+        // Your code here 
+        int temp = 0;
+        for(int j = i-1; j>=0 ; j--) {
+            if(arr[i] < arr[j]) {
+                 temp = arr[i];
+                 arr[i] = arr[j];
+                 arr[j] = temp;
+                }
+            temp = 0;
+            i--;
+        }
+                
+        
+    }
+    //Function to sort the array using insertion sort algorithm.
+    void insertionSort(int arr[], int n)
+    {
+        //code here
+        for(int i=0; i<n; i++) {
+            
+          insert(arr,i);  
+                    
+        }
+    }
+}
