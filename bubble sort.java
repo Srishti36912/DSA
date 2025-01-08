414990a3a0f13fbfe219e23e58395cf1b640a9fd
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-01-08 15:52:50 +0530

    bubble sort

---
+++ bubble sort.java
@@ -0,0 +1,20 @@
+class bubblesort {
+    //User function Template for C
+//Function to sort the array using bubble sort algorithm.
+void bubbleSort(int arr[], int n)
+{
+    // Your code here 
+    int temp=0;
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n-i-1; j++) {
+            if(arr[j] > arr[j+1]) {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+}
+
+
