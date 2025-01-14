ab65ea9ff7f0f5b42c90b6a875f75982712123ae
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-01-14 15:19:22 +0530

    second largest element

---
+++ array/secondLargest.java
@@ -0,0 +1,25 @@
+package array;
+
+public class secondLargest {
+    // User function template for C++
+class Solution {
+      // Function returns the second
+      // largest elements
+      public int getSecondLargest(int []arr) {
+          // Code Here
+          int max = -1;
+          int sec_max = -1;
+          int n = arr.length;
+          for(int i=0; i<n; i++) {
+              if(arr[i] > max) {
+                  sec_max = max;
+                  max = arr[i];
+              }
+              else if(arr[i] > sec_max && arr[i] != max) {
+                      sec_max = arr[i];
+                  }
+          }
+          return sec_max;
+      }
+  }
+}
