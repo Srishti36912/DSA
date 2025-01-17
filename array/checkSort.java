0dc350e7efde38aaeb841a0a496fff5a55711f51
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-01-17 19:06:08 +0530

    check if the array is sorted

---
+++ array/checkSort.java
@@ -0,0 +1,11 @@
+package array;
+public class checkSort {
+    public boolean check(int[] nums) {
+        for(int i=1; i< nums.length; i++) {
+            if (nums[i] < nums[i-1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
