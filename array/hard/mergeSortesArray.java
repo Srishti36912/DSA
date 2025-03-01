3862b085c0a09a9a4539f0fe831367abc5130e03
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-03-01 21:12:42 +0530

    merging 2 sorted arrays and returning the first array only

--- array/hard/mergeSortesArray.java
+++ array/hard/mergeSortesArray.java
@@ -20,5 +20,30 @@ public class mergeSortesArray {
             }
         }
         Arrays.sort(nums1);
+        Arrays.sort(nums1);
+    }
+
+    // if we have to return only the nums1 array and remove 0s from it
+    
+    public void mergeForNums1(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1, j = n - 1, k = n + m - 1;
+
+        while(i >= 0 && j >= 0) {
+            if(nums1[i] >= nums2[j]) {
+                nums1[k] = nums1[i];
+                k--;
+                i--;
+            }
+            else {
+                nums1[k] = nums2[j];
+                k--;
+                j--;
+            }
+        }
+        while(j >= 0) {
+            nums1[k] = nums2[j];
+            k--;
+            j--;
+        }
     }
 }
