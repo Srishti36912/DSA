507879ce9d0dc1b8c531f2cdea09c0a5b689b81c
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-03-08 00:01:10 +0530

    converting array to linked list

---
+++ LinkedList/oneD_LinkedList/ArrayToLL.java
@@ -0,0 +1,25 @@
+package LinkedList.oneD_LinkedList;
+
+class Node {
+    int data;
+    Node next;
+
+    Node() { data = 0; }
+    Node(int d) { data = d; }  //constructor to create a new node
+}
+
+public class ArrayToLL {
+    static Node constructLL(int arr[]) {
+        // code here
+        if(arr.length == 0) return null;
+        
+        Node head = new Node(arr[0]);
+        Node current = head;
+        
+        for(int i=1; i<arr.length; i++) {
+            current.next = new Node(arr[i]);
+            current = current.next;
+        }
+        return head;
+    }
+}
