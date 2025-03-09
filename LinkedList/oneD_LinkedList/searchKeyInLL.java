8978ab31d77e7c58b7cf2494b8e72529753fe3cf
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-03-09 16:11:14 +0530

    search for an element in the linked list

---
+++ LinkedList/oneD_LinkedList/searchKeyInLL.java
@@ -0,0 +1,21 @@
+package LinkedList.oneD_LinkedList;
+
+class Node {
+    int data;
+     Node next;
+     Node(int d)  { data = d;  next = null; }
+ }
+public class searchKeyInLL {
+    static boolean searchKey(int n, Node head, int key) {
+        // Code here
+        Node current = head;
+        
+        for(int i=0; i<n; i++) {
+            if(current.data == key) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+}
