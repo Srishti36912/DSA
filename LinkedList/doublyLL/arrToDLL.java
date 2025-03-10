ba199f640db2986b80f7504482a3f550676f1388
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-03-10 19:01:55 +0530

    converting array to doubly linked list

---
+++ LinkedList/doublyLL/arrToDLL.java
@@ -0,0 +1,26 @@
+package LinkedList.doublyLL;
+
+class Node {
+    int data;
+    Node prev;
+    Node next;
+    Node(int d)  { data = d;  prev = null; next = null; }
+}
+
+public class arrToDLL {
+    Node constructDLL(int arr[]) {
+        // Code here
+        Node head = new Node(arr[0]);
+        
+        Node temp = head;
+        temp.prev = null;
+        
+        for(int i=1; i<arr.length; i++) {
+            Node current = new Node(arr[i]);
+            current.prev = temp;
+            temp.next = current;
+            temp = current;
+        }
+        return head;
+    }
+}
