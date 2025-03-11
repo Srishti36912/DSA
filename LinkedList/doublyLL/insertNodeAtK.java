8b92b5ead98d8971c84e9552bb59d481c83683b8
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-03-11 16:25:39 +0530

    insert node at k position

---
+++ LinkedList/doublyLL/insertNodeAtK.java
@@ -0,0 +1,40 @@
+package LinkedList.doublyLL;
+
+class Node
+{
+    int data;
+    Node next;
+    Node prev;
+    Node(int data)
+    {
+        this.data = data;
+        next = prev = null;
+    }
+}
+
+public class insertNodeAtK {
+    Node addNode(Node head, int p, int x) {
+        // Your code here
+        Node newNode = new Node(x);
+        Node current = head;
+        int i = 0;
+        
+        if(head == null) {
+            return newNode;
+        }
+        
+        while(current.next != null && i<p) {
+                i++;
+                current = current.next;
+        }
+        
+        if(current == null) {
+            return head;
+        }
+        newNode.next = current.next;
+        newNode.prev = current;
+        current.next = newNode;
+        
+        return head;
+    }
+}
