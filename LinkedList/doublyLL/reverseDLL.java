a69a499fd03f247ebf63d460eefa7feec299025f
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-03-12 13:52:47 +0530

    reversing a doubly linked list

---
+++ LinkedList/doublyLL/reverseDLL.java
@@ -0,0 +1,33 @@
+package LinkedList.doublyLL;
+
+class DLLNode {
+    int data;
+    DLLNode next;
+    DLLNode prev;
+
+    DLLNode(int val) {
+        data = val;
+        next = null;
+        prev = null;
+    }
+}
+
+public class reverseDLL {
+    public DLLNode reverseDLL(DLLNode head) {
+        // Your code here
+        if(head == null || head.next == null) {
+            return head;
+        }
+        DLLNode current = head;
+        DLLNode prev = null;
+        while(current != null) {
+            prev = current.prev;
+            current.prev = current.next;
+            current.next = prev;
+            current = current.prev;
+        }
+        
+        return prev.prev;
+        
+    }
+}
