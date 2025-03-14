4d6871ca2cd9ffb99d60b796b1bdc23ee3e48b38
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-03-14 16:52:19 +0530

    finding the middle of the linked list

---
+++ LinkedList/mediumLL/middleOfLL.java
@@ -0,0 +1,23 @@
+package LinkedList.mediumLL;
+
+class ListNode {
+         int val;
+         ListNode next;
+         ListNode() {}
+         ListNode(int val) { this.val = val; }
+         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     }
+
+public class middleOfLL {
+    public ListNode middleNode(ListNode head) {
+        ListNode hare = head;
+        ListNode tortoise = head;
+
+        while(hare != null && tortoise != null && hare.next != null) {
+            hare = hare.next.next;
+            tortoise = tortoise.next;
+        }
+
+        return tortoise;
+    }
+}
