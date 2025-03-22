ba2578488f2451b31c6118f603d1fff565c48c5c
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-03-22 23:43:55 +0530

    seggregate even and odd indexes

--- LinkedList/mediumLL/middleOfLL.java
+++ LinkedList/mediumLL/middleOfLL.java
@@ -7,7 +7,7 @@ class ListNode {
          ListNode(int val) { this.val = val; }
          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
      }
-
+     
 public class middleOfLL {
     public ListNode middleNode(ListNode head) {
         ListNode hare = head;
---
+++ LinkedList/mediumLL/segEvenOddIndex.java
@@ -0,0 +1,30 @@
+package LinkedList.mediumLL;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class segEvenOddIndex {
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null || head.next == null) {
+            return head;
+        }
+        ListNode odd = head;
+        ListNode even = head.next; 
+        ListNode evenHead = even;
+
+        while(even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = even.next.next;
+            even = even.next;
+        }
+
+        odd.next = evenHead;
+        return head;
+    }
+}
