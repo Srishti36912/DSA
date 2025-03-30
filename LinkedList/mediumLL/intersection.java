32120d99aafbbb8c46607c3550206ae350ee8f1b
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-03-30 16:03:56 +0530

    optimised and concised solution

--- LinkedList/mediumLL/intersection.java
+++ LinkedList/mediumLL/intersection.java
@@ -1,65 +1,15 @@
-static Node insertNode(Node head,int val) {
-    Node newNode = new Node(val);
-    
-    if(head == null) {
-        head = newNode;
-        return head;
-    }
-    
-    Node temp = head;
-    while(temp.next != null) temp = temp.next;
-    
-    temp.next = newNode;
-    return head;
-}
+package LinkedList.mediumLL;
 
-//utility function to check presence of intersection
-static Node intersectionPresent(Node head1,Node head2) {
-    while(head2 != null) {
-        Node temp = head1;
-        while(temp != null) {
-            //if both nodes are same
-            if(temp == head2) return head2;
-            temp = temp.next;
+class intersection {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode d1 = headA;
+        ListNode d2 = headB;
+    
+        while(d1 != d2) {
+            d1 = d1 == null ? headB : d1.next;
+            d2 = d2 == null ? headA : d2.next;
         }
-        head2 = head2.next;
-    }
-    //intersection is not present between the lists return null
-    return null;
-}
-
-//utility function to print linked list created
-static void printList(Node head) {
-    while(head.next != null) {
-    System.out.print(head.num+"->");
-        head = head.next;
-    }
-    System.out.println(head.num);
-}
-
-public static void main(String args[]) {
-    // creation of both lists 
-    Node head = null;
-    head=insertNode(head,1);
-    head=insertNode(head,3);
-    head=insertNode(head,1);
-    head=insertNode(head,2);
-    head=insertNode(head,4);
-    Node head1 = head;
-    head = head.next.next.next;
-    Node headSec = null;
-    headSec=insertNode(headSec,3);
-    Node head2 = headSec;
-    headSec.next = head;
-    //printing of the lists
-    System.out.print("List1: "); printList(head1);
-    System.out.print("List2: "); printList(head2);
-    //checking if intersection is present
-    Node answerNode = intersectionPresent(head1,head2);
-    if(answerNode == null)  
-    System.out.println("No intersection\n");
-    else
-    System.out.println("The intersection point is "+answerNode.num);
     
-}
+        return d1;
+    }
 }
