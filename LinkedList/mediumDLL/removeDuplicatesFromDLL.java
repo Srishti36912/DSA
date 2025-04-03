c27a658ecc044d357aabe8925d8e104d22fb0c39
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-04-03 15:38:18 +0530

    removing duplicates from sorted doubly linked lists

---
+++ LinkedList/mediumDLL/removeDuplicatesFromDLL.java
@@ -0,0 +1,24 @@
+package LinkedList.mediumDLL;
+
+public class removeDuplicatesFromDLL {
+    Node removeDuplicates(Node head){
+        // Code Here.
+        Node temp = head.next;
+        
+        if(head == null || head.next == null) {
+            return head;
+        }
+        
+        while(temp != null) {
+            if(temp.prev.data == temp.data) {
+                temp.prev.next = temp.next;
+                
+                if(temp.next != null) {
+                    temp.next.prev = temp.prev;
+                }
+            }
+            temp = temp.next;
+        }
+        return head;
+    }
+}
