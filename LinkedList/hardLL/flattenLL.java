ab69cedf2ca9f560bc5760712ecec98a32158cb8
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-04-05 23:40:59 +0530

    flattening a linked list

---
+++ LinkedList/hardLL/flattenLL.java
@@ -0,0 +1,38 @@
+package LinkedList.hardLL;
+
+public class flattenLL {
+    Node flatten(Node root) {
+        // code here
+        if(root == null || root.next == null) {
+            return root;
+        }
+        
+        Node mergeRoot = flatten(root.next);
+        return merge(root, mergeRoot);
+    }
+    
+    Node merge(Node head, Node mergeHead) {
+        Node dummyNode = new Node(-1);
+        Node res = dummyNode;
+        
+        while(head != null && mergeHead != null) {
+            if(head.data < mergeHead.data) {
+                res.bottom = head;
+                res = head;
+                head = head.bottom;
+            } else {
+                res.bottom = mergeHead;
+                res = mergeHead;
+                mergeHead = mergeHead.bottom;
+            }
+            res.next = null;
+        }
+        if(head != null) {
+            res.bottom = head;
+        } else {
+            res.bottom = mergeHead;
+        }
+        
+        return dummyNode.bottom;
+    }
+}
