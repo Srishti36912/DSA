48983516845d18be7592b980abc0fe0435dcfabc
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-04-07 20:12:40 +0530

    implementing stack using arrays

---
+++ stack queue/learning/stackUsingArray.java
@@ -0,0 +1,27 @@
+package stack queue.learning;
+
+public class stackUsingArray {
+    private int[] arr;
+    private int top;
+
+    public MyStack() {
+        arr = new int[1000];
+        top = -1;
+    }
+
+    public void push(int x) {
+        // Your Code
+        top++;
+        arr[top] = x;
+    }
+
+    public int pop() {
+        // Your Code
+        if(top == -1) {
+            return -1;
+        }
+        int x = arr[top];
+        top--;
+        return x;
+    }
+}
