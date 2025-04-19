3be6ee87492a26d213d9647031230be02f85086d
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-04-20 00:14:46 +0530

    optimal solution

--- stack_queue/monotonic stack/nextGreaterElement.java
+++ stack_queue/monotonic stack/nextGreaterElement.java
@@ -1,23 +1,19 @@
+package stack_queue.monotonic_stack;
+
 public class nextGreaterElement {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         HashMap<Integer, Integer> m = new HashMap<>();
         Stack<Integer> st = new Stack<>();
         int n = nums1.length;
         int nge[] = new int[n];
-        for(int i=nums2.length-1; i>=0; i++) {
-            while(! st.empty() && st.peek() < nums1[i]) {
-                st.pop();
+        for(int num : nums2) {
+            while(!st.isEmpty() &&  num > st.peek()) {
+                m.put(st.pop(), num);
             }
-            if(st.empty()) {
-                m[nums2[i]] = -1;
-            }
-            else {
-                m[nums2[i]] = st.peek();
-            }
-            st.push(nums2[i]);
+            st.push(num);
         }
         for(int i=0; i<n; i++) {
-            nge[i] = map
+            nge[i] = m.getOrDefault(nums1[i], -1);
         }
         return nge;
     }
