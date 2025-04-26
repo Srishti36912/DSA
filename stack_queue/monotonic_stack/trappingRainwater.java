2c98f105bfe21917d3a510ed9545609e3680fe72
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-04-26 17:54:47 +0530

    finding the units of rainwater trapped between the buildings

---
+++ stack_queue/monotonic_stack/trappingRainwater.java
@@ -0,0 +1,29 @@
+package stack_queue.monotonic_stack;
+
+public class trappingRainwater {
+    public int trap(int[] height) {
+        int n = height.length;
+        int total = 0, lMax = 0, rMax = 0;
+        int l = 0, r = n-1;
+
+        while(l < r) {
+            if(height[l] < height[r]) {
+                if(lMax > height[l]) {
+                    total += lMax - height[l];
+                } else {
+                    lMax = height[l];
+                }
+                l++;
+            }
+            else {
+                if(rMax > height[r]) {
+                    total += rMax - height[r];
+                } else {
+                    rMax = height[r];
+                }
+                r--;
+            }
+        }
+        return total;
+    }
+}
