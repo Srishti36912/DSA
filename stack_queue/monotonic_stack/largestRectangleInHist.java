3c66bcb6ba93cf540c0f056623df285b5924f393
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-04-30 23:01:02 +0530

    largest rectangle in histogram

---
+++ stack_queue/monotonic_stack/largestRectangleInHist.java
@@ -0,0 +1,15 @@
+package stack_queue.monotonic_stack;
+
+public class largestRectangleInHist {
+    static int largestarea(int arr[], int n) {
+        int maxArea = 0;
+        for (int i = 0; i < n; i++) {
+            int minHeight = Integer.MAX_VALUE;
+            for (int j = i; j < n; j++) {
+                minHeight = Math.min(minHeight, arr[j]);
+                maxArea = Math.max(maxArea, minHeight * (j - i + 1));
+            }
+        }
+        return maxArea;
+    }
+}
