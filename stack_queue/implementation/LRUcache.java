0d5ef42076aeb1688737acf065617f260eedaf02
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-05-07 13:52:10 +0530

    modified the solution

--- stack_queue/implementation/LRUcache.java
+++ stack_queue/implementation/LRUcache.java
@@ -3,58 +3,63 @@ package stack_queue.implementation;
 import java.util.HashMap;
 import java.util.Map;
 
+class Node {
+    int key;
+    int value;
+    Node next;
+    Node prev;
+    public Node(int _key, int _value) {
+        key = _key;
+        value = _value;
+    }
+}
+
 public class LRUcache {
-    Node head = new Node(0, 0), tail = new Node(0, 0);
-    Map < Integer, Node > map = new HashMap();
+    HashMap<Integer, Node> mpp = new HashMap<>();
     int capacity;
+    Node head = new Node(0,0), tail = new Node(0,0);
 
     public LRUCache(int _capacity) {
         capacity = _capacity;
+        mpp.clear();
         head.next = tail;
         tail.prev = head;
     }
-
+    
     public int get(int key) {
-        if (map.containsKey(key)) {
-            Node node = map.get(key);
-            remove(node);
-            insert(node);
+        if(mpp.containsKey(key)) {
+            Node node = mpp.get(key);
+            deleteNode(node);
+            insertAfterHead(node);
             return node.value;
-        } else {
+        } 
+        else {
             return -1;
         }
     }
-
+    
     public void put(int key, int value) {
-        if (map.containsKey(key)) {
-            remove(map.get(key));
+        if(mpp.containsKey(key)) {
+            Node node = mpp.get(key);
+            deleteNode(node);
         }
-        if (map.size() == capacity) {
-            remove(tail.prev);
+        if(mpp.size() == capacity) {
+            Node node = tail.prev;
+            deleteNode(node);
         }
-        insert(new Node(key, value));
+        Node node = new Node(key, value);
+        insertAfterHead(node);
     }
-
-    private void remove(Node node) {
-        map.remove(node.key);
+    private void deleteNode(Node node) {
+        mpp.remove(node.key);
         node.prev.next = node.next;
         node.next.prev = node.prev;
     }
-
-    private void insert(Node node) {
-        map.put(node.key, node);
+    private void insertAfterHead(Node node) {
+        mpp.put(node.key, node);
         node.next = head.next;
         node.next.prev = node;
         head.next = node;
         node.prev = head;
-    }
-
-    class Node {
-        Node prev, next;
-        int key, value;
-        Node(int _key, int _value) {
-            key = _key;
-            value = _value;
-        }
     }
 }
