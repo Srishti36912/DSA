cdd1567b4693f9fe369b329c096cc874c67511c4
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-07-11 22:32:55 +0530

    finding the position of the element in the array

---
+++ binarySearch/BS_ON_1D/searchInsertPos.java
@@ -0,0 +1,22 @@
+package binarySearch.BS_ON_1D;
+
+public class searchInsertPos {
+    public int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        int low = 0;
+        int high = n - 1;
+        int ans = n;
+
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            if(nums[mid] >= target) {
+                ans = mid;
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
