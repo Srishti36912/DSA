9bdac9fb0d66e84fb6faacc834133be0f2e0656b
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-07-16 00:17:25 +0530

    finding the target in the rotated sorted array with duplicate values

---
+++ binarySearch/BS_ON_1D/searchRotatedSortedArrayII.java
@@ -0,0 +1,41 @@
+package binarySearch.BS_ON_1D;
+
+public class searchRotatedSortedArrayII {
+    public boolean search(int[] nums, int target) {
+        int n = nums.length;
+        int low = 0, high = n - 1;
+
+        while(low <= high) {
+            int mid = (low + high) / 2;
+
+            if(target == nums[mid]) {
+                return true;
+            }
+
+            if(nums[low] == nums[mid] && nums[mid] == nums[high]) {
+                low = low + 1;
+                high = high - 1;
+                continue;
+            }
+
+            if(nums[low] <= nums[mid]) {
+                if(nums[low] <= target && target <= nums[mid]) {
+                    high = mid - 1;
+                }
+                else {
+                    low = mid + 1;
+                }
+            }
+            else {
+                 if(nums[mid] <= target && target <= nums[high]) {
+                    low = mid + 1;
+                }
+                else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+}
