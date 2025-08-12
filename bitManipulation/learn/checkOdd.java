936c54dbc83e3d600f4d490c50a3ca3c3f3dd79a
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-08-12 23:26:57 +0530

    changed the code

--- bitManipulation/learn/checkOdd.java
+++ bitManipulation/learn/checkOdd.java
@@ -2,9 +2,9 @@ package bitManipulation.learn;
 
 public class checkOdd {
     
-    static boolean isEven(int n) {
+    static boolean isOdd(int n) {
         // code here
         int a = n&1;
-        return (a == 1) ? false : true;
+        return (a == 1) ? true : false;
     }
 }
