31874947b363998d9645ff71f5e8b964ef2a25e6
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-08-18 23:09:34 +0530

    finding the minimum number of bits to be flipped to convert A to B

---
+++ bitManipulation/learn/minFlipBits.java
@@ -0,0 +1,16 @@
+package bitManipulation.learn;
+
+public class minFlipBits {
+    
+    public int minBitFlips(int start, int goal) {
+        int xor = start ^ goal;
+        int count = 0;
+
+        while(xor != 0) {
+            count += xor & 1;
+            xor >>= 1;
+        }
+
+        return count;
+    }
+}
