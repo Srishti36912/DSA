28d6c9bb25ba45361681bfc11b19dda25ced2185
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-08-19 19:48:17 +0530

    finding the single number that is not repeated in the array

---
+++ bitManipulation/interviewProblems/singleNumber.java
@@ -0,0 +1,12 @@
+package bitManipulation.interviewProblems;
+
+public class singleNumber {
+    
+    public int singleNumber(int[] nums) {
+        int xor = 0;
+        for(int i=0; i<nums.length; i++) {
+            xor = xor ^ nums[i];
+        }
+        return xor;
+    }
+}
