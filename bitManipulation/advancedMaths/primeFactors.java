1ebc19c0fe5f512a7b4e559f1e79d49b5c268d81
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-08-23 17:56:55 +0530

    finding all the unique prime factors of given number

---
+++ bitManipulation/advancedMaths/primeFactors.java
@@ -0,0 +1,23 @@
+package bitManipulation.advancedMaths;
+
+import java.util.ArrayList;
+
+public class primeFactors {
+    
+    public static ArrayList<Integer> primeFac(int n) {
+        // code here
+        ArrayList<Integer> result = new ArrayList<>();
+        
+        for(int i=2; i<=n; i++) {
+            if(n % i == 0) {
+                result.add(i);
+            }
+            
+            while(n % i == 0) {
+            n = n / i;
+            }
+        }
+        
+        return result;
+    }
+}
