10ca237833623e7df213805d19f1f8584a24e0b2
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-08-24 17:23:58 +0530

    finding all divisors of a number

---
+++ bitManipulation/advancedMaths/allDivisors.java
@@ -0,0 +1,29 @@
+package bitManipulation.advancedMaths;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class allDivisors {
+    
+    public static void print_divisors(int n) {
+        // code here
+        ArrayList<Integer> ans = new ArrayList<>();
+        int sqrtN = (int) Math.sqrt(n);
+        
+        for(int i=1; i<=sqrtN; i++) {
+            if(n % i == 0) {
+                ans.add(i);
+                
+                if(i != n/i) {
+                    ans.add(n/i);
+                }
+            }
+        }
+        
+        Collections.sort(ans);
+        
+        for(int divisor: ans) {
+            System.out.print(divisor + " ");
+        }
+    }
+}
