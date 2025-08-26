40b635120644a872dba798102e044464f5a98c30
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-08-26 23:03:41 +0530

    finding the count of prime numbers that are less than the given number n

---
+++ bitManipulation/advancedMaths/sieveOfEratosthenes.java
@@ -0,0 +1,26 @@
+package bitManipulation.advancedMaths;
+
+public class sieveOfEratosthenes {
+    
+    public int countPrimes(int n) {
+       if(n < 3) {
+        return 0;
+       }
+
+       boolean[] flag = new boolean[n];
+       int count = n / 2;
+
+       for(int i=3; i * i< n; i+=2) {
+            if(!flag[i]) {
+                for(int j = i*i; j<n; j+= 2 * i) {
+                    if(!flag[j]) {
+                        flag[j] = true;
+                        count--;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+}
