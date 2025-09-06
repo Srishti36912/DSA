dea9a8062939a8d4825e02afa96488f72202bcdf
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-09-06 17:09:13 +0530

    finding the maximum number of consecutive ones if we can flip k zeros only

---
+++ slidingWindow_twoPointers/medium/maxConsecutivesOnesIII.java
@@ -0,0 +1,23 @@
+package slidingWindow_twoPointers.medium;
+
+public class maxConsecutivesOnesIII {
+    
+    public int longestOnes(int[] nums, int k) {
+       int left = 0, len = 0, count = 0;
+
+       for(int right = 0; right<nums.length; right++) {
+            if(nums[right] == 0) {
+                count++;
+            }
+            while(count > k) {
+                if(nums[left] == 0) {
+                    count--;
+                }
+                left++;
+            }
+
+            len = Math.max(len, right- left + 1);
+        }
+        return len;
+    }
+}
